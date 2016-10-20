3ca25efba008545c66a845b2f8fe27a9954e93f0
charlesjamb <charles.jambon@gmail.com>
2016-10-20 14:23:09 -0400

    rewrote everything with promises

--- demo-using-api.js
+++ demo-using-api.js
@@ -1,38 +1,46 @@
-// load the mysql library
 var mysql = require('mysql');
 
-// create a connection to our Cloud9 server
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'ziad_saab', // CHANGE THIS :)
-  password : '',
-  database: 'reddit'
+  user     : 'root',
+  password : 'sqltemppassword',
+  database : 'reddit'
 });
 
-// load our API and pass it the connection
-var reddit = require('./reddit');
+var reddit = require('./reddit.js');
 var redditAPI = reddit(connection);
 
-// It's request time!
-redditAPI.createUser({
-  username: 'hello23',
-  password: 'xxx'
-}, function(err, user) {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    redditAPI.createPost({
-      title: 'hi reddit!',
-      url: 'https://www.reddit.com',
+function newUserAndpost() {
+  redditAPI.createUser({
+    username: 'hello6',
+    password: 'xxxx'
+    })
+  .then(function(user) {
+    console.log(user);
+
+    return redditAPI.createPost({
+      title: 'hi reddit',
+      url: 'https://reddit.com',
       userId: user.id
-    }, function(err, post) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log(post);
-      }
-    });
-  }
-});
+    })
+  })
+  .then(function(post) {
+    console.log(post);
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+}
+
+function allPosts() {
+  redditAPI.getAllPosts()
+  .then(function(result) {
+    console.log(result);
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+}
+
+newUserAndpost();
+allPosts();
---
+++ library/basicpromises.js
@@ -0,0 +1,93 @@
+var request = require('request');
+var prompt = require('prompt');
+var mysql = require('mysql');
+var bcrypt = require('bcrypt');
+var HASH_ROUNDS = 10;
+
+// hash a password with bcrypt
+function crypt(password, HASH_ROUNDS) {
+	return new Promise(function(resolve, reject) {
+		bcrypt.hash(password, HASH_ROUNDS, function(err, hashedPassword) {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(hashedPassword);
+			}
+		})
+	});
+}
+
+// query to the database
+function makeConnQuery(connection) {
+	return function connQuery(thequery, params) {
+		return new Promise(function(resolve, reject) {
+			connection.query(thequery, params, function(err, result) {
+				if (err) {
+					reject(err);
+				}
+				else {
+					resolve(result);
+				}
+			})
+		});
+	}	
+}
+
+
+// stringify the sql query
+function niceQuery(query) {
+	return (
+		connectionQuery(query)
+		.then(function(result) {
+			var actualQuery = JSON.stringify(result, null, 4);
+			return actualQuery;
+		})
+	);
+}
+
+// request promise
+function requestPromise(url) {
+	return new Promise(function(resolve, reject) {
+		request(url, function(err, result) {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(result);
+			}
+		})
+	});
+}
+
+// prompt promise
+function promptPromise(question) {
+	return new Promise(function(resolve, reject) {
+		prompt.get(question, function(err, answer) {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(answer);
+			}
+		})
+	});
+}
+
+// request as JSON
+function requestJSON(url) {
+	return ( 
+		requestPromise(url)
+		.then(function(result) {
+			var actualResult = JSON.parse(result.body);
+			return actualResult;
+		})
+	);	
+}
+
+exports.crypt = crypt;
+exports.makeConnQuery = makeConnQuery;
+exports.requestPromise = requestPromise;
+exports.promptPromise = promptPromise;
+exports.requestJSON = requestJSON;
+exports.niceQuery = niceQuery; 
