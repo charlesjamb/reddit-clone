3fd01d8c95bcdedda118045fdb7c793a8dd53ea2
charlesjamb <charles.jambon@gmail.com>
2016-10-24 14:20:50 -0400

    exercise 1

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
+app.get('/hello', function(req, res) {
+	res.send('<h1>Hello World</h1>');
+});
 
 
 
@@ -12,9 +15,12 @@ app.get('/', function (req, res) {
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen((process.env.PORT || 3000), (process.env.IP || '127.0.0.1'), function () {
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 });
+
+// On browser
+// http://127.0.0.1:3000/
