c4e7b011c8c73657d5ae46fd7bac6e9d172d968d
charlesjamb <charles.jambon@gmail.com>
2016-10-24 16:16:36 -0400

    starting ex4

--- index.js
+++ index.js
@@ -11,7 +11,35 @@ app.get('/hello', function(req, res) {
 	res.send('Hello ' + req.query.name);
 });
 
+app.get('/hello/:name', function(req, res) {
+	res.send('Hello ' + req.params.name);
+});
 
+app.get('/calculator/:operation', function(req, res) {
+	var theCalculator = {
+		'operator': req.params.operation,
+		'firstOperand': parseInt(req.query.num1),
+		'secondOperand': parseInt(req.query.num2)
+	};
+
+	switch(theCalculator.operator) {
+		case 'add':
+			theCalculator.solution = theCalculator.firstOperand + theCalculator.secondOperand;
+			break;
+		case 'sub':
+			theCalculator.solution = theCalculator.firstOperand - theCalculator.secondOperand;
+			break;
+		case 'mult':
+			theCalculator.solution = theCalculator.firstOperand * theCalculator.secondOperand;
+			break;
+		case 'div':
+			theCalculator.solution = theCalculator.firstOperand / theCalculator.secondOperand;
+			break;
+		default:
+		res.sendStatus(400);	
+	}
+	res.send(JSON.stringify(theCalculator, null, 4));
+});
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
